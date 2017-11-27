d750e4e490fa6882bf7e65b6258d332104f8f30e
thw912 <1970751106@qq.com>
2017-11-27 19:42:30 +0800

    add a note

--- src/clock/Main.java
+++ src/clock/Main.java
@@ -2,6 +2,7 @@
 
 public class Main {
 
+	//这是main函数程序的入口
 	public static void main(String[] args) {
 		java.util.Scanner in = new java.util.Scanner(System.in);
 		Clock clock = new Clock(in.nextInt(), in.nextInt(), in.nextInt());
